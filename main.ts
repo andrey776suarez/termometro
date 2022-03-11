5bb62c844a53fd701b943099289eeeded7e3c3e7
andrey776suarez <99951372+andrey776suarez@users.noreply.github.com>
2022-03-11 08:56:11 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    serial.writeLine("" + (input.temperature()))
+    serial.writeLine("celsius")
+    basic.showString("" + (input.temperature()))
+    basic.showString("celsius")
+})
+input.onButtonPressed(Button.AB, function () {
+    if (input.temperature() < 25) {
+        serial.writeLine("Agafar jaqueta")
+        basic.showIcon(IconNames.Umbrella)
+    }
+    if (input.temperature() > 25) {
+        serial.writeLine("No agafar jaqueta")
+    }
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        # # # # #
+        . # # # .
+        # . # . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    if (input.temperature() > 25) {
+        serial.writeLine("" + (input.temperature()))
+        basic.showIcon(IconNames.Happy)
+    } else if (input.temperature() < 25) {
+        serial.writeLine("" + (input.temperature()))
+        basic.showIcon(IconNames.Sad)
+    }
+})
+basic.forever(function () {
+	
+})
